b89962ca93ba921667d3a79b05d43ae3bbf1bc64
Ivan Klochkov <vany-nov13@yandex.ru>
2024-12-01 22:56:43 -0800

    refactor: move burger in separate file

---
+++ christmas-shop/pages/gifts/gifts.js
@@ -0,0 +1,15 @@
+import Burger from '../../js/burger.js'
+
+const burger = new Burger()
+document.addEventListener('DOMContentLoaded', () => {
+	burger.bindListeners()
+})
+
+function removeBurger() {
+	const display = window.innerWidth
+	if (display > 768) {
+		burger.clickLink()
+	}
+}
+
+window.addEventListener('resize', removeBurger)
