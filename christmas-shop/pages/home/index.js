5435fc6bcd7d1dc07a65e7d0535673f366444995
Ivan Klochkov <vany-nov13@yandex.ru>
2024-12-04 13:30:04 -0800

    feat: add random cards on main page

--- christmas-shop/pages/home/index.js
+++ christmas-shop/pages/home/index.js
@@ -1,12 +1,41 @@
+import itemsFromJson from '../../data/items.json' with { type: 'json' }
 import Burger from '../../js/burger.js'
+import Menu from '../../js/menuPars.js'
 import Slider from '../../js/slider.js'
 import Timer from '../../js/timer.js'
 
 const burger = new Burger()
 const slider = new Slider()
+const menu = new Menu()
 
 document.addEventListener('DOMContentLoaded', () => {
 	burger.bindListeners()
 	slider.bindListeners()
 	setInterval(Timer, 1000)
+
+	let items = []
+	for (let i = 0; i < itemsFromJson.length; i += 1) {
+		items.push(
+			new Menu(
+				itemsFromJson[i]['name'],
+				itemsFromJson[i]['description'],
+				itemsFromJson[i]['img'],
+				itemsFromJson[i]['superpowers'],
+				itemsFromJson[i]['category'].toLowerCase()
+			)
+		)
+	}
+	let itemsList = document.querySelector('.best__menu')
+
+	function randomItems(arr) {
+		itemsList.innerHTML = ''
+		let item
+		for (let i = 0; i < 4; i += 1) {
+			let cardsNum = Math.floor(Math.random() * 36)
+			item = arr[cardsNum].insertHTML(i)
+			itemsList.append(item)
+		}
+	}
+
+	randomItems(items)
 })
