123dfb65f63213c83d0143d3fa1bcec9c1458c05
Ivan Klochkov <vany-nov13@yandex.ru>
2024-12-11 23:53:32 -0800

    fix: update close modal bug

--- christmas-shop/js/modal.js
+++ christmas-shop/js/modal.js
@@ -9,16 +9,17 @@ function toggleModal(item, id) {
 		modal.innerHTML = ''
 		modal.classList.remove('visible')
 		return
+	} else {
+		document.body.style.overflowY = 'hidden'
+		modal.classList.add('visible')
+		modal.append(item.insertHTMLModalWindow(id))
+		modal.querySelector('.close-btn').addEventListener('click', toggleModal)
+		modal.addEventListener('click', e => {
+			if (e.target.matches('.menu__modal.visible')) {
+				toggleModal()
+			}
+		})
 	}
-	document.body.style.overflowY = 'hidden'
-	modal.classList.add('visible')
-	modal.append(item.insertHTMLModalWindow(id))
-	modal.querySelector('.close-btn').addEventListener('click', toggleModal)
-	modal.addEventListener('click', e => {
-		if (e.target.matches('.menu__modal')) {
-			toggleModal()
-		}
-	})
 }
 
 // toggleModal(menu, 0)
