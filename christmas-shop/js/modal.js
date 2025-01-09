555218fd76dcccd15aa49d036102c97e2c2be18a
Ivan Klochkov <vany-nov13@yandex.ru>
2025-01-08 16:14:47 -1000

    refactor: remove comments

--- christmas-shop/js/modal.js
+++ christmas-shop/js/modal.js
@@ -1,6 +1,3 @@
-import Menu from '../js/menuPars.js'
-
-let menu = new Menu()
 let modal = document.querySelector('.menu__modal')
 
 function toggleModal(item, id) {
@@ -22,21 +19,4 @@ function toggleModal(item, id) {
 	}
 }
 
-// toggleModal(menu, 0)
-
 export default toggleModal
-
-// function updateSnowflakes(superpowers) {
-//   const updateSnowflake = (snowflakeContainer, value) => {
-//       const snowflakes = snowflakeContainer.querySelectorAll('svg');
-//       const numBright = Math.max(0, Math.min(5, Math.ceil((parseInt(value.replace('+', '')) / 100))));
-//       const numPale = 5 - numBright;
-
-//       snowflakes.forEach((snowflake, index) => {
-//         if (index < numBright) {
-//             snowflake.style.opacity = 1;
-//         } else {
-//             snowflake.style.opacity = 0.1;
-//         }
-//     });
-// };
